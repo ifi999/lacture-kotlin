ae64bb249539549a49955faeb7cf446f5aa311c3
ifi999 <kkwkjm4231@gmail.com>
2025-10-03 16:55:13 +0900

    add: error code 추가

--- src/main/kotlin/common/exception/ErrorCode.kt
+++ src/main/kotlin/common/exception/ErrorCode.kt
@@ -9,5 +9,7 @@ enum class ErrorCode(
     override val code: Int,
     override var message: String
 ) : CodeInterface {
-    AUTH_CONFIG_NOT_FOUND(-100, "auth config not found")
+    AUTH_CONFIG_NOT_FOUND(-100, "auth config not found"),
+    FAILED_TO_CALL_CLIENT(-101, "Failed to call client"),
+    CALL_RESULT_BODY_NULL(-102, "body is null"),
 }
