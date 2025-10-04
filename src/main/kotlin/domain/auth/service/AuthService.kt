3946bfb6be4be351cfc4de7d6263a6d25a2b2229
ifi999 <kkwkjm4231@gmail.com>
2025-10-04 22:52:56 +0900

    add: AuthService 구현

--- src/main/kotlin/domain/auth/service/AuthService.kt
+++ src/main/kotlin/domain/auth/service/AuthService.kt
@@ -1,3 +1,25 @@
 package org.example.domain.auth.service
 
+import org.example.common.exception.CustomException
+import org.example.common.exception.ErrorCode
+import org.example.common.jwt.JwtProvider
+import org.example.interfaces.OAuthServiceInterface
+import org.springframework.stereotype.Service
 
+@Service
+class AuthService(
+    private val oauth2Services: Map<String, OAuthServiceInterface>,
+    private val jwtProvider: JwtProvider
+) {
+    fun handleAuth(state : String, code: String) : String {
+        val provider = state.lowercase()
+
+        val callService = oauth2Services[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
+
+        val accessToken = callService.getToken(code)
+        val userInfo = callService.getUserInfo(accessToken.accessToken)
+        val token = jwtProvider.createToken(provider, userInfo.email, userInfo.name, userInfo.id)
+
+        return token
+    }
+}
