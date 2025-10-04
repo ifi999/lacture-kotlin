6751fbad75722205a55b9a63c43ebf80fa9588a2
ifi999 <kkwkjm4231@gmail.com>
2025-10-04 22:06:43 +0900

    add: 플랫폼별 getToken 일부 구현

--- src/main/kotlin/domain/auth/service/GithubAuthService.kt
+++ src/main/kotlin/domain/auth/service/GithubAuthService.kt
@@ -1,7 +1,9 @@
 package org.example.domain.auth.service
 
+import okhttp3.FormBody
 import org.example.common.exception.CustomException
 import org.example.common.exception.ErrorCode
+import org.example.common.httpClient.CallClient
 import org.example.config.Oauth2Config
 import org.example.interfaces.OAuth2TokenResponse
 import org.example.interfaces.OAuth2UserResponse
@@ -12,13 +14,30 @@ private const val key = "github"
 
 @Service(key)
 class GithubAuthService(
-    private val config: Oauth2Config
+    private val config: Oauth2Config,
+    private val httpClient: CallClient
 ) : OAuthServiceInterface {
 
     private val oAuthInfo = config.providers[key] ?: throw CustomException(ErrorCode.AUTH_CONFIG_NOT_FOUND, key)
+    private val tokenURl = "https://github.com/login/oauth/access_token"
+    private val userInfoURL = "https://api.github.com/user"
+
     override val providerName: String = key
 
     override fun getToken(code: String): OAuth2TokenResponse {
+        val body = FormBody.Builder()
+            .add("code", code)
+            .add("client_id", oAuthInfo.clientId)
+            .add("client_secret", oAuthInfo.clientSecret)
+            .add("redirect_uri", oAuthInfo.redirectUri)
+            .add("grant_type", "authorization_code")
+            .build()
+
+        val headers = mapOf("Accept" to "application/json")
+        val jsonString = httpClient.POST(tokenURl, headers, body)
+
+        // jsoonString -> json 처리
+
         TODO("Not yet implemented")
     }
 
