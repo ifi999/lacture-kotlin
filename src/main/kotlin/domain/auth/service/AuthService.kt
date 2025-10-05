7dd47f2de3c9414c168eaa959c863ac5d73048b2
ifi999 <kkwkjm4231@gmail.com>
2025-10-05 22:57:06 +0900

    fix: AuthService handleAuth 추가 구현

--- src/main/kotlin/domain/auth/service/AuthService.kt
+++ src/main/kotlin/domain/auth/service/AuthService.kt
@@ -1,18 +1,29 @@
 package org.example.domain.auth.service
 
+import com.github.f4b6a3.ulid.UlidCreator
 import org.example.common.exception.CustomException
 import org.example.common.exception.ErrorCode
 import org.example.common.jwt.JwtProvider
+import org.example.common.logging.Logging
+import org.example.common.transaction.Transactional
+import org.example.domain.auth.repository.AuthUserRepository
 import org.example.interfaces.OAuthServiceInterface
+import org.example.types.entity.User
+import org.slf4j.Logger
 import org.springframework.stereotype.Service
 
 @Service
 class AuthService(
     private val oauth2Services: Map<String, OAuthServiceInterface>,
-    private val jwtProvider: JwtProvider
+    private val jwtProvider: JwtProvider,
+    private val logger: Logger = Logging.getLogger(AuthService::class.java),
+    private val transaction : Transactional,
+    private val authUserRepository: AuthUserRepository
 ) {
-    fun handleAuth(state : String, code: String) : String {
+
+    fun handleAuth(state : String, code: String) : String = Logging.logFor(logger) { log ->
         val provider = state.lowercase()
+        log["provider"] = provider
 
         val callService = oauth2Services[provider] ?: throw CustomException(ErrorCode.PROVIDER_NOT_FOUND, provider)
 
@@ -20,6 +31,24 @@ class AuthService(
         val userInfo = callService.getUserInfo(accessToken.accessToken)
         val token = jwtProvider.createToken(provider, userInfo.email, userInfo.name, userInfo.id)
 
-        return token
+        val username = (userInfo.name ?: userInfo.email).toString()
+
+        transaction.run {
+            val exist = authUserRepository.existsByUsername(username)
+
+            if (exist) {
+                // access token update
+                authUserRepository.updateAccessTokenByUserName(username, token)
+            } else {
+                val ulid = UlidCreator.getUlid().toString()
+
+                val user = User(ulid, username, token)
+
+                authUserRepository.save(user)
+            }
+        }
+
+        return@logFor token
     }
+
 }
