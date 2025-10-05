4ab17ae4dd427d27fb264bfc1f38f02321a13775
ifi999 <kkwkjm4231@gmail.com>
2025-10-05 22:56:24 +0900

    chore: ulid 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,9 @@ dependencies {
     // client
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
 
+    // ulid
+    implementation("com.github.f4b6a3:ulid-creator:5.2.3")
+
     testImplementation("org.jetbrains.kotlin:kotlin-test")
 }
 
