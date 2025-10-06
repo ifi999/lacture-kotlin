403f5c7b19e79643ba695b7ace3497ba730aa942
ifi999 <kkwkjm4231@gmail.com>
2025-10-06 22:06:17 +0900

    fix: 쿼리 매개변수 명칭 수정

--- src/main/kotlin/domain/auth/repository/AuthUserRepository.kt
+++ src/main/kotlin/domain/auth/repository/AuthUserRepository.kt
@@ -11,7 +11,7 @@ interface AuthUserRepository : JpaRepository<User, String> {
     fun existsByUsername(username : String) : Boolean
 
     @Modifying
-    @Query("UPDATE User SET accessToken = :token WHERE username = :username")
+    @Query("UPDATE User SET accessToken = :accessToken WHERE username = :username")
     fun updateAccessTokenByUserName(
         @Param("username") username: String,
         @Param("accessToken") token: String
