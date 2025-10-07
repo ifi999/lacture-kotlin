087f4e378e80a1a03fa70023996c63f3c05438b9
ifi999 <kkwkjm4231@gmail.com>
2025-10-07 21:38:26 +0900

    add: BankUserRepository 클래스 작성

---
+++ src/main/kotlin/domain/bank/repository/BankUserRepository.kt
@@ -0,0 +1,10 @@
+package org.example.domain.bank.repository
+
+import org.example.types.entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface BankUserRepository : JpaRepository<User, String> {
+
+    fun findByUlid(ulid : String) : User?
+
+}
