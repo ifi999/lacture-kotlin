5fbcef0a147595016dad745d0ef80cd327737ede
ifi999 <kkwkjm4231@gmail.com>
2025-10-09 23:16:11 +0900

    fix: findByUlid null 허용 제거

--- src/main/kotlin/domain/bank/repository/BankUserRepository.kt
+++ src/main/kotlin/domain/bank/repository/BankUserRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface BankUserRepository : JpaRepository<User, String> {
 
-    fun findByUlid(ulid : String) : User?
+    fun findByUlid(ulid : String) : User
 
 }
